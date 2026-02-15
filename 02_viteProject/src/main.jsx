ce0dfb6692879571b4d7040197b141b14d4f996a
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-02-15 16:41:24 +0000

    In this main.jsx file, i create a function to check its direct run or not becuase at the end in this <App /> file are also run like Function. 1. It runs using directCheck() so it also run like that we learn. 2. second learn is :- create element and ditect inject in other function. 3. third :- React.createElement is a method to create ReactElement direct in 4 step (in our project) 1st - tag, 2nd - in {} attribute, 3rd - title that show on DOM, 4rd - inject globel variable direct to inject with Text

--- 02_viteProject/src/main.jsx
+++ 02_viteProject/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function directCheck() {
+  return (
+    <div>
+      <h2>Let's Check Run or Not</h2>
+    </div>
+  )
+}
+
+const a_Element = (
+  <a href='https:///google.com'>Visit Site</a>
+)
+
+const anotherUser = ' Goku'
+
+const reactElement = React.createElement( // react element library run like this & it's a object
+  'a',                    // Element / Tag
+  { href: 'https://google.com', target: '_blank' },     // Attributes {}
+  'Click here to visit',  // Title
+  anotherUser             // Inject Variable Goku
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  // directCheck() // its run but we cannot execute like that
+  // <App />
+  reactElement // it also run
+
 )
