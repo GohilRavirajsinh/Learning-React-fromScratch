4b0d6bbad5b81b7c31da4c3736c5897454963479
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-02-17 11:28:24 +0000

    understand how to stateFunction and stateVariable call, How difference between simple variable and stateVariable!

--- 03_counter/src/App.jsx
+++ 03_counter/src/App.jsx
@@ -3,33 +3,36 @@ import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(15) // let [variable, function(callback - check how's code condition)] = useState(), // Jitne bhi states hoti hai '(setCounter)' vo el callback function accept karta hai.
+
   // let counter = 15 // In React ye default sabhi jgah pe update nhin krage
-  let [counter, setCounter] = useState(15) // let [variable, method] = useState()
 
   const incressValue = () => {
-    if (counter >= 0 && counter < 20) { // task counter cannot count greate than 20
-      counter = counter + 1
-    }
-    setCounter(counter)
+    // counter =+ 1
+    // setCounter(counter + 1) // ese bhi hota hai
+    setCounter(preCounter => preCounter + 1)       // 'callback' ye sahi tarika hai kyunki last me ye 'setCounter()' hai to function hi. iske ander react ka alag se stateVariable hota hai jo value ko UI me update karta hai!
+    // without curly {} '{} lgayenge to value return karni pdegi' or parantheses ke bina bhi chalta hai ye!
+    // setCounter(preCounter => preCounter + 1) 
+    // setCounter(preCounter => preCounter + 1)
+    // setCounter(preCounter => preCounter + 1) // Note: Agar yha par setCounter(counter => counter + 1) ye likhenge to ye counter or uper vala dono counter alag hote hai, so variable updatation me dikkat na ho uske liye better hai stateVaraible ka name hi alag se likh den!
+    // setCounter(preCounter => preCounter + 1) jinti bar ye call krenge utni bar ye counting incress hogi ye hai stateVariable ka kmal
   }
 
   const decressValue = () => {
-    if (counter > 0 && counter <= 20) { // task counter cannot count less than 0
-      counter = counter - 1
-    }
-    setCounter(counter)
+    setCounter(preCounter => preCounter - 1)
   }
 
   return (
     <>
-      <h1>Increment and Decrement</h1>
-      <h2>Counter value: {counter}</h2>
+      <h1>Goku & Jinhoo</h1>
+      <h2>Counter Value : {counter}</h2>
 
-      <button onClick={incressValue}>Incress value {counter}</button>
+      <button onClick={incressValue}>incress value {counter}</button>
       <br /> <br />
-      <button onClick={decressValue}>Decress value {counter}</button>
+      <button onClick={decressValue}>decress value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
 
-export default App
+export default App;
