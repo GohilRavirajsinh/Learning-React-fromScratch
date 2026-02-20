1e2073656410dc265aac2266c9796c822406bd6d
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-02-20 20:57:03 +0000

    custom Hooks and components

---
+++ 07_currencyConverter/src/components/index.js
@@ -0,0 +1,8 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+// jitne bhi components hote hai ham un components ko yha late hai or yhan se export kerte hai kyunki varna ye InputBox, SearchBox ye bhot kuchh ban jata hai.
+// to hmm index file me se component folder ko hi import karva denge or fir ye index file se hi sabko access kar lenge.
+// ab sirf ek hi file import karna rahega or sab file ka access yhan se le sakte hai. 
+// :: Ye mendatary nhin hai karna na karna hmare uper hai!
